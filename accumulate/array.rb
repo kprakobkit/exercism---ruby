ea1ab15fe4941eb6a10dc50e3af8b56503d21429
Peter Prakobkit <kprakobkit@gmail.com>
2014-12-15 21:36:50 -0800

    refactor solution

--- accumulate/array.rb
+++ accumulate/array.rb
@@ -1,9 +1,5 @@
 class Array
   def accumulate &block
-    result = []
-
-    each { |element| result << (yield element) }
-
-    result
+    reduce([]) { |result, element| result << (yield element) }
   end
 end
