41c4d54651b0b65024ae77505008383aacf96308
Peter Prakobkit <kprakobkit@gmail.com>
2014-12-16 21:47:53 -0800

    refactor sum.rb

--- sum-of-multiples/sum.rb
+++ sum-of-multiples/sum.rb
@@ -4,10 +4,16 @@ class SumOfMultiples
   end
 
   def self.to(n)
-   (0...n).select { |num| num % 3 == 0 || num % 5 == 0 }.reduce(:+)
+    new(3,5).to(n)
   end
 
   def to(n)
-    (0...n).select { |num| @multiples.any? { |multiple| num % multiple == 0 } }.reduce(:+)
+    (0...n).select { |num| is_multiple? num }.reduce(:+)
+  end
+
+  private
+
+  def is_multiple?(num)
+    @multiples.any? { |multiple| num % multiple == 0 }
   end
 end
