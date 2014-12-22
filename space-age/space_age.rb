5410d28bc178a749cef4b30d9b05b3974f10e23b
Peter Prakobkit <kprakobkit@gmail.com>
2014-12-21 16:28:11 -0800

    refactor

--- space-age/space_age.rb
+++ space-age/space_age.rb
@@ -5,34 +5,40 @@ class SpaceAge
   end
 
   def on_earth
-    (@seconds / 31557600.to_f).round(2)
+    format_seconds(@seconds / 31557600.00)
   end
 
   def on_mercury
-    (on_earth / 0.2408467).round(2)
+    format_seconds(on_earth / 0.2408467)
   end
 
   def on_venus
-    (on_earth / 0.61519726).round(2)
+    format_seconds(on_earth / 0.61519726)
   end
 
   def on_mars
-    (on_earth / 1.8808158).round(2)
+    format_seconds(on_earth / 1.8808158)
   end
 
   def on_jupiter
-    (on_earth / 11.862615).round(2)
+    format_seconds(on_earth / 11.862615)
   end
 
   def on_saturn
-    (on_earth / 29.447498).round(2)
+    format_seconds(on_earth / 29.447498)
   end
 
   def on_uranus
-    (on_earth / 84.016846).round(2)
+    format_seconds(on_earth / 84.016846)
   end
 
   def on_neptune
-    (on_earth / 164.79132).round(2)
+    format_seconds(on_earth / 164.79132)
+  end
+
+  private
+
+  def format_seconds(seconds)
+    seconds.round(2)
   end
 end
