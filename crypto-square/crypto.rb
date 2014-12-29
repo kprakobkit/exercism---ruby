ecf7f8ea95df48b01623632f0cc3bb03ecbbc299
Peter Prakobkit <kprakobkit@gmail.com>
2014-12-29 11:17:25 -0800

    refactor

--- crypto-square/crypto.rb
+++ crypto-square/crypto.rb
@@ -6,8 +6,7 @@ class Crypto
 
   def size
     message_length = @normalize_text.length
-    size = is_perfect_square?(message_length) ? message_length : find_next_perfect_square(message_length)
-    Math.sqrt(size).to_i
+    Math.sqrt(message_length).ceil
   end
 
   def normalize_plaintext
@@ -19,30 +18,13 @@ class Crypto
   end
 
   def ciphertext
-    cipher = []
-    size.times do |n|
-      cipher[n] = ""
-      plaintext_segments.each do |word|
-        cipher[n] += word[n] if word[n]
-      end
+    size.times.reduce("") do |result, n|
+      result << plaintext_segments.map { |word| word[n]}.join
     end
-    cipher.join
   end
 
   def normalize_ciphertext
     rows = (@normalize_text.length / size.to_f).ceil
     ciphertext.chars.each_slice(rows).map(&:join).join(" ")
-  end
-
-  private
-
-  def is_perfect_square?(number)
-    squared = Math.sqrt(number)
-    squared.to_i ** 2 == number
-  end
-
-  def find_next_perfect_square(number)
-    number += 1 until is_perfect_square?(number)
-    number
   end
 end
